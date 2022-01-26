5a23ce69d47bf7bb9e7502c28954df827c66c1d9
Jenson Collins <jenson.collins@hotmail.com>
2022-01-27 08:31:53 +0900

    Test script

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,41 +2,61 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract Staking {
     IERC20 public stakingToken;
 
+    uint public constant DIVISION = 10000;
+
     uint public lastRewardRate; // S = 0;
-    uint private totalStakedAmount; // T = 0;
-    mapping(address => uint) private stakedBalance; // stake = {};
-    mapping(address => uint) private rewardRate; // S0 = {};
+    uint public totalStakedAmount; // T = 0;
+    mapping(address => uint) public stakedBalance; // stake = {};
+    mapping(address => uint) public rewardRate; // S0 = {};
 
     constructor (address _stakingToken) {
         stakingToken = IERC20(_stakingToken);
     }
 
-    function deposit(address account, uint _amount) external {
-        stakedBalance[account] += _amount; // stake[address] = amount;
-        rewardRate[account] = lastRewardRate; // S0[address] = S;
+    function stake(uint _amount) external {
+        require(_amount > 0);
+        uint allowance = stakingToken.allowance(msg.sender, address(this));
+        require(allowance >= _amount, "Check the token allowance");
+        stakedBalance[msg.sender] += _amount; // stake[address] = amount;
+        rewardRate[msg.sender] = lastRewardRate; // S0[address] = S;
         totalStakedAmount += _amount; // T = T + amount;
-        stakingToken.transferFrom(account, address(this), _amount);
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
     }
 
     function distribute(uint reward) external {
         require(totalStakedAmount != 0, ""); // if T==0 then revert();
-        lastRewardRate += reward / totalStakedAmount; // S = S + r / T;
+        uint allowance = stakingToken.allowance(msg.sender, address(this));
+        require(allowance >= reward, "Check the token allowance");
+        stakingToken.transferFrom(msg.sender, address(this), reward);
+        /// r / t can be under 0
+        lastRewardRate += (reward * DIVISION / totalStakedAmount); // S = S + r / T;
     }
 
-    function withdraw(address account) external {
-        uint deposited = stakedBalance[account]; // deposited = stake[address];
-        uint reward = deposited * (lastRewardRate - rewardRate[account]); // reward = deposited * (S - S0[address]);
+    function unstake(uint _amount) external {
+        uint deposited = stakedBalance[msg.sender]; // deposited = stake[address];
+        require(_amount <= deposited, "Withdraw amount is invalid");
+        uint reward = deposited * (lastRewardRate - rewardRate[msg.sender]) / DIVISION; // reward = deposited * (S - S0[address]);
+        rewardRate[msg.sender] = lastRewardRate;
+        totalStakedAmount -= _amount; // T = T - deposited;
+        stakedBalance[msg.sender] -= _amount; // stake[address] = 0;
+        stakingToken.transfer(msg.sender, _amount + reward);
+    }
+
+    function unstakeAll() external {
+        uint deposited = stakedBalance[msg.sender]; // deposited = stake[address];
+        uint reward = deposited * (lastRewardRate - rewardRate[msg.sender]) / DIVISION; // reward = deposited * (S - S0[address]);
         totalStakedAmount -= deposited; // T = T - deposited;
-        stakedBalance[account] = 0; // stake[address] = 0;
-        stakingToken.transfer(account, deposited + reward);
-        return deposited + reward; // return deposited + reward
+        stakedBalance[msg.sender] = 0; // stake[address] = 0;
+        stakingToken.transfer(msg.sender, deposited + reward);
     }
 
-    function getReward(address account) public view {
-
+    function getReward(address account) public view returns (uint){
+        uint deposited = stakedBalance[account]; // deposited = stake[address];
+        return deposited * (lastRewardRate - rewardRate[account]) / DIVISION; // reward = deposited * (S - S0[address]);
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,75 @@
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
+import { BigNumber } from "@ethersproject/bignumber";
+
+export const ether = (amount: number | string): BigNumber => {
+  const weiString = ethers.utils.parseEther(amount.toString());
+  return BigNumber.from(weiString);
+};
 
 describe("Staking", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Staking = await ethers.getContractFactory("Staking");
-    const stakingSC = await Staking.deploy("Hello, world!");
+  let mockToken: Contract;
+  let stakingSC: Contract;
+  let signers: any;
+
+  beforeEach(async () => {
+    signers = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token", signers[0]);
+    mockToken = await Token.deploy();
+    await mockToken.deployed();
+
+    const Staking = await ethers.getContractFactory("Staking", signers[0]);
+    stakingSC = await Staking.deploy(mockToken.address);
     await stakingSC.deployed();
 
-    expect(await stakingSC.greet()).to.equal("Hello, world!");
+    await mockToken.connect(signers[0]).mint(signers[0].address, ether(1000));
+  });
+
+  describe("Staking & Distribution", async () => {
+    it("Staking", async () => {
+      await mockToken.connect(signers[0]).approve(stakingSC.address, ether(100));
+      await stakingSC.connect(signers[0]).stake(ether(100));
+      const afterBalance = await mockToken.balanceOf(signers[0].address);
+      expect(afterBalance).to.eq(ether(900));
+    });
+
+    it("Getting Reward", async () => {
+      await mockToken.connect(signers[0]).approve(stakingSC.address, ether(150));
+      await stakingSC.connect(signers[0]).stake(ether(100));
+
+      let reward = await stakingSC.getReward(signers[0].address);
+      expect(reward).to.eq(0);
+
+      await stakingSC.connect(signers[0]).distribute(ether(50));
+      reward = await stakingSC.getReward(signers[0].address);
+      expect(reward).to.eq(ether(50));
+    });
+
+    it("Distribution", async () => {
+      await mockToken.connect(signers[0]).approve(stakingSC.address, ether(150));
+      await stakingSC.connect(signers[0]).stake(ether(100));
+      await stakingSC.connect(signers[0]).distribute(ether(50));
+
+      const lastRewardRate = await stakingSC.lastRewardRate();
+      expect(lastRewardRate).to.eq(BigNumber.from("5000"));
+    });
+
+    it("Unstake", async () => {
+      await mockToken.connect(signers[0]).approve(stakingSC.address, ether(150));
+      await stakingSC.connect(signers[0]).stake(ether(100));
+      await stakingSC.connect(signers[0]).distribute(ether(50));
+
+      await stakingSC.connect(signers[0]).unstake(ether(50));
+      let afterUnstakeBalance = await stakingSC.stakedBalance(
+        signers[0].address
+      );
+      expect(afterUnstakeBalance).to.eq(ether(50));
+
+      await stakingSC.connect(signers[0]).unstakeAll();
+      afterUnstakeBalance = await stakingSC.stakedBalance(signers[0].address);
+      expect(afterUnstakeBalance).to.eq(ether(0));
+    });
   });
 });
