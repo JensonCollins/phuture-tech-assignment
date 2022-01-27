2b72f20835aab7f73651b686de8ab76230712f63
Jenson Collins <jenson.collins@hotmail.com>
2022-01-27 19:22:10 +0900

    1. Removed allowance checking 2. calculating after transfer

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -20,18 +20,14 @@ contract Staking {
 
     function stake(uint _amount) external {
         require(_amount > 0);
-        uint allowance = stakingToken.allowance(msg.sender, address(this));
-        require(allowance >= _amount, "Check the token allowance");
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
         stakedBalance[msg.sender] += _amount; // stake[address] = amount;
         rewardRate[msg.sender] = lastRewardRate; // S0[address] = S;
         totalStakedAmount += _amount; // T = T + amount;
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
     }
 
     function distribute(uint reward) external {
         require(totalStakedAmount != 0, ""); // if T==0 then revert();
-        uint allowance = stakingToken.allowance(msg.sender, address(this));
-        require(allowance >= reward, "Check the token allowance");
         stakingToken.transferFrom(msg.sender, address(this), reward);
         /// r / t can be under 0
         lastRewardRate += (reward * DIVISION / totalStakedAmount); // S = S + r / T;
@@ -41,18 +37,18 @@ contract Staking {
         uint deposited = stakedBalance[msg.sender]; // deposited = stake[address];
         require(_amount <= deposited, "Withdraw amount is invalid");
         uint reward = deposited * (lastRewardRate - rewardRate[msg.sender]) / DIVISION; // reward = deposited * (S - S0[address]);
+        stakingToken.transfer(msg.sender, _amount + reward);
         rewardRate[msg.sender] = lastRewardRate;
         totalStakedAmount -= _amount; // T = T - deposited;
         stakedBalance[msg.sender] -= _amount; // stake[address] = 0;
-        stakingToken.transfer(msg.sender, _amount + reward);
     }
 
     function unstakeAll() external {
         uint deposited = stakedBalance[msg.sender]; // deposited = stake[address];
         uint reward = deposited * (lastRewardRate - rewardRate[msg.sender]) / DIVISION; // reward = deposited * (S - S0[address]);
+        stakingToken.transfer(msg.sender, deposited + reward);
         totalStakedAmount -= deposited; // T = T - deposited;
         stakedBalance[msg.sender] = 0; // stake[address] = 0;
-        stakingToken.transfer(msg.sender, deposited + reward);
     }
 
     function getReward(address account) public view returns (uint){
